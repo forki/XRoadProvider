b20bf8c01856854b9c3933ab789da7401ffa373a
Janno Põldma <janno.poldma@gmail.com>
2015-07-18 11:03:16 +0300

    Note for future self.

--- src/XRoadProvider/XRoad.ErasedProducerDefinition.fs
+++ src/XRoadProvider/XRoad.ErasedProducerDefinition.fs
@@ -6,6 +6,9 @@ open XRoad.Common
 
 let baseTy = typeof<obj>
 
+// TODO: Erased type provider would benefit if schema was loaded and parsed lazily.
+//       Current implementation loads and parses entire schema before provider initializes.
+
 let initProducerMembers (producerTy: ProvidedTypeDefinition) uri languageCode =
     let schema = ProducerDescription.Load(resolveUri uri, languageCode)
     let typesTy = ProvidedTypeDefinition("Types", Some baseTy, HideObjectMethods=true)
@@ -13,14 +16,15 @@ let initProducerMembers (producerTy: ProvidedTypeDefinition) uri languageCode =
     schema.Services
     |> List.map (fun svc ->
         let serviceTy = ProvidedTypeDefinition(svc.Name, Some baseTy, HideObjectMethods=true)
-        svc.Ports
-        |> List.map (fun prt ->
-            let portTy = ProvidedTypeDefinition(prt.Name, Some baseTy, HideObjectMethods=true)
-            let ctr = ProvidedConstructor([])
-            ctr.InvokeCode <- fun _ -> <@@ obj() @@>
-            portTy.AddMember(ctr)
-            portTy)
-        |> serviceTy.AddMembers
+        serviceTy.AddMembersDelayed(fun _ ->
+            svc.Ports
+            |> List.map (fun prt ->
+                let portTy = ProvidedTypeDefinition(prt.Name, Some baseTy, HideObjectMethods=true)
+                let ctr = ProvidedConstructor([])
+                ctr.InvokeCode <- fun _ -> <@@ obj() @@>
+                portTy.AddMember(ctr)
+                portTy)
+        )
         serviceTy)
     |> producerTy.AddMembers
     ()
